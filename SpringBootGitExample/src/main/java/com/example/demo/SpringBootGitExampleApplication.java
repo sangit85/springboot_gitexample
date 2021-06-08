315b759af6a57327a4fa322c37d3a2e5c49f3435
sangit85 <santoshk.github@gmail.com>
2021-06-08 10:24:18 +0530

    commit helloworld

---
+++ SpringBootGitExample/src/main/java/com/example/demo/SpringBootGitExampleApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitExampleApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
